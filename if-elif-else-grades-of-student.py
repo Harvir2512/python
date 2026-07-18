cb6fc3b16f6330c41a740b0b1c8c25e0fc04bfc1
Harvir2512 <harveervarma10@gmail.com>
2026-07-18 07:16:13 -0700

    Add files via upload

---
+++ if-elif-else-grades-of-student.py
@@ -0,0 +1,13 @@
+
+"""
+marks=int(input("enter your marks :"))
+if(marks>=90):
+    print("A grade")
+elif(marks>=80 and marks<90):
+    print("B grade")
+elif(marks>=70 and marks<80):
+    print("C grade")
+elif(marks>=60 and marks<70):
+    print("D grade")
+else :
+    print("fill")"""
